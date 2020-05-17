62c65cb70c95a36b11b141d81b5bab78db27acff
Jacob Stanger <jstangwork@gmail.com>
2020-05-17 16:15:31 -0600

    attempt to implement redux

---
+++ client/src/actions/index.js
@@ -0,0 +1,7 @@
+let nextTodoId = 0;
+
+export const addTodo = text => ({
+  type: 'ADD_TODO',
+  id: nextTodoId++,
+  text
+});
--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,17 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import rootReducer from './reducers';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}> 
+    <App />
+  </Provider>,
+  document.getElementById("root"));
+
 registerServiceWorker();
