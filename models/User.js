e17fb5471784ecf5eed30abbba6fe0eb824ad7b8
Jacob Stanger <jstangwork@gmail.com>
2020-05-22 12:25:13 -0600

    cleanup comments

--- models/User.js
+++ models/User.js
@@ -14,24 +14,4 @@ const UserSchema = new Schema({
 
 const User = mongoose.model('users', UserSchema);
 
-// ./index.js is handling the exports
 module.exports = User;
-
-// from stackoverflow
-// var userSchema = new Schema({
-//     twittername: String,
-//     twitterID: Number,
-//     displayName: String,
-//     profilePic: String,
-// });
-
-// var  User = mongoose.model('User')
-
-
-// /* USERS */
-// /* 1 */
-// {
-//     "_id" : ObjectId("5ec2c3bd74eb1c1a41714151"),
-//     "username" : "hello_me_hi",
-//     "password" : "123"
-// }
