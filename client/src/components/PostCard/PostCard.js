0f565d1eadcb60d41b8e79fba8d8082ea1f2f116
Brian Allen <brianallen2401@gmail.com>
2020-05-21 18:14:56 -0600

    added some titles etc for readability. Added a favicon

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -5,15 +5,16 @@ export default function PostCard(props) {
   const community = props.post.onCommunity.name;
   const postedBy = props.post.postedBy.displayName;
   const title = props.post.title;
-  console.log(props);
 
   return (
     <div className="card card-fluid">
       <h1 className="card-header">{title}</h1>
       <div className="card-body">
         <p>{body}</p>
-        <p>{community}</p>
-        <p>{postedBy}</p>
+        <div className="row">
+          <p className="mr-2">Community: {community} </p>
+          <p>Post Created by: {postedBy}</p>
+        </div>
       </div>
     </div>
   );
