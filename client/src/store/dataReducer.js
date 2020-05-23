0c931da5174adef4141d714df87b5e97bf5cb2d9
Jacob Stanger <jstangwork@gmail.com>
2020-05-22 18:32:03 -0600

    attempt at new reducer for redux.

---
+++ client/src/store/dataReducer.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+const getFreshData = () => {
+  console.log('inside getFreshData()');
+  axios.get('/api/all-posts')
+    .then(docs => {
+      return docs;
+    })
+    .catch(err => {
+      console.log(err.message);
+      return [];
+    });
+  // return [];
+}
+
+const initialState = {
+  defaultImgUrl: "",
+  numberOfCommunities: 1,
+  numberOfPosts: 1,
+  numberOfUsers: 1,
+  postList: getFreshData()
+}
+
+const listReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'refreshData':
+      const posts = [];
+      posts = getFreshData();
+      console.log('here is posts data: ' + posts);
+      return {
+        ...state,
+        postList: posts
+      }
+    default:
+      break;
+  }
+  return state;
+}
+
+
+export default listReducer;
