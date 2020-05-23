edfcefa0670d35c723cb8359b958140da3c80e71
Brian Allen <brianallen2401@gmail.com>
2020-05-23 10:45:18 -0600

    fixed alignment issue in header

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -6,7 +6,7 @@ export default function PostCard(props) {
   console.log(img);
   return (
     <div className="card mt-1 text-center col-sm-8">
-      <img src={img} className="card-img-top card-img " alt="..." />
+      <img src={img} className="card-img-top card-img" alt="..." />
       <div className="card-footer">
         <small className="text-muted">Likes: </small>
       </div>
