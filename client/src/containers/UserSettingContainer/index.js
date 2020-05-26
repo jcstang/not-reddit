91e3305087d4ca671c004f48087277f04ab0dc82
Jacob Stanger <jstangwork@gmail.com>
2020-05-26 12:11:39 -0600

    adding bootstrap comps and files

--- client/src/containers/UserSettingContainer/index.js
+++ client/src/containers/UserSettingContainer/index.js
@@ -1,27 +1,44 @@
 import UserSettingItem from "../../components/UserSettingItem";
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 
 
 export default function UserSettingContainer(props) {
   return (
-    <div className="container-fluid">
-      <UserSettingItem
-        user={props.user}
-        itemText={"@username"}
-        valueText={props.user.username}
-      />
-      <UserSettingItem
-        user={props.user}
-        itemText={"displayName"}
-        valueText={props.user.displayName}
-      />
-      <UserSettingItem
-        user={props.user}
-        itemText={"email"}
-        valueText={props.user.email}
-      />
-    </div>
+    <Container>
+      <Row>
+        <Col md={8}>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+              Your name may appear around GitHub where you contribute or are mentioned. You can remove it at any time.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+        <Col md={4}>
+          <p><strong>Profile Picture</strong></p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
