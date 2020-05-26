90cb2e7942f247b469cf9405d31e11ad6e4eaef5
Jacob Stanger <jstangwork@gmail.com>
2020-05-26 09:56:07 -0600

    user setting container

---
+++ client/src/containers/UserSettingContainer/index.js
@@ -0,0 +1,25 @@
+import UserSettingItem from "../../components/UserSettingItem";
+import React from 'react';
+
+
+export default function UserSettingContainer(props) {
+  return (
+    <div className="container-fluid">
+      <UserSettingItem 
+        user={props.user}
+        itemText={"@username"}
+        valueText={props.user.username}
+      />
+      <UserSettingItem 
+        user={props.user}
+        itemText={"displayName"}
+        valueText={props.user.displayName}
+      />
+      <UserSettingItem 
+        user={props.user}
+        itemText={"email"}
+        valueText={props.user.email}
+      />
+    </div>
+  );
+}
