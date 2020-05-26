27ecc60566f3e89480db6cb4abe61cfc0cd10936
Jacob Stanger <jstangwork@gmail.com>
2020-05-26 17:02:16 -0600

    onchange not working

--- client/src/containers/UserSettingContainer/index.js
+++ client/src/containers/UserSettingContainer/index.js
@@ -1,5 +1,5 @@
+import React, { useState, useEffect, useReducer } from 'react';
 import UserSettingItem from "../../components/UserSettingItem";
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,26 +8,79 @@ import Button from 'react-bootstrap/Button';
 import './index.css';
 
 
+const userSettingsReducer = (state, action) => {
+  if (action.type === 'bear') {
+    return {
+      ...state,
+      username: action.username
+    }
+  }
+
+  return state;
+}
 
 
 export default function UserSettingContainer(props) {
+  const placeholderImg = "https://source.unsplash.com/6anudmpILw4/200x200";
+
+
+  const [ userNameState, setUserNameState ] = useState('');
+  const [ userEmailState, setUserEmailState ] = useState('');
+  const [ userDisplayNameState, setUserDisplayNameState ] = useState('');
+
+  const [ userSettingState, userSettingDispatch ] = useReducer(userSettingsReducer, {
+    username: "JimHalpert23",
+    email: "jim@dundermifflin.com",
+    displayName: "Jim Halpert"
+  });
+
+  // useEffect(() => {
+  //   setUserNameState(props.user.username || "JimHalpert23");
+  //   userSettingDispatch({type: 'bear', username: props.user.username});
+  //   setUserEmailState(props.user.email || "jim@dundermifflin.com");
+  //   setUserDisplayNameState(props.user.displayName || "Jim Halpert");
+  // });
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    // TODO: stuff
+  }
+
+  const onChangeHandler = (event) => {
+    console.log(event);
+  }
+
   return (
+
     <Container>
       <Row>
         <Col md={8}>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control className="thing" type="email" placeholder="Enter email" />
+          <Form onSubmit={onSubmitHandler}>
+
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control 
+                className="thing" 
+                type="text" 
+                placeholder="Joe Dirt" 
+                value={userNameState} 
+                onChange={event => setUserNameState(event.target.value)}
+              />
               <Form.Text className="text-muted">
                 Your name may appear around GitHub where you contribute or are mentioned. You can remove it at any time.
               </Form.Text>
             </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control className="thing" type="password" placeholder="Password" />
+          
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control className="thing" type="email" placeholder="Enter email" value={userEmailState} />
             </Form.Group>
+
+            <Form.Group controlId="formBasicDisplayName">
+              <Form.Label>Display Name</Form.Label>
+              <Form.Control className="thing" type="text" placeholder="Display Name" value={userDisplayNameState} />
+            </Form.Group>
+
             <Button 
               variant="primary" 
               type="submit"
@@ -35,15 +88,17 @@ export default function UserSettingContainer(props) {
               >
               Submit
             </Button>
+
           </Form>
         </Col>
         <Col md={4}>
           <p><strong>Profile Picture</strong></p>
           <div>
-            <img src="https://source.unsplash.com/6anudmpILw4/200x200"></img>
+            <img src={props.user.imageUrl}></img>
           </div>
         </Col>
       </Row>
     </Container>
+
   );
 }
---
+++ client/src/pages/SearchPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SearchPage() {
+    return (
+        <div className="container">
+            <h2>SearchPage</h2>
+        </div>
+    );
+}
