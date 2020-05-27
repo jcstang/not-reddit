0d952674bed0e929bab61c8c85bb4d69f9bc5ad1
Brian Allen <brianallen2401@gmail.com>
2020-05-26 19:06:40 -0600

    minor style changes. Trying to get the cards to render more dynamically

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -4,7 +4,7 @@ import './PostCard.css';
 export default function PostCard(props) {
   const img = props.image;
   return (
-    <div className="card mt-1 text-center col-sm-8 col-md-5 col-lg-3">
+    <div className="card mt-1 col-sm-4">
       <img src={img} className="card-img-top card-img" alt="..." />
       <div className="card-footer">
         <small className="text-muted">Likes: </small>
