ca3f45c0134674fab7e20d60e36dec1501c5f99b
grambo <gramham@live.com>
2020-05-28 13:55:10 -0600

    made the main viewer into a modal

--- client/src/components/PostViewer/PostViewer.js
+++ client/src/components/PostViewer/PostViewer.js
@@ -14,30 +14,37 @@ class PostViewer extends Component {
     }
   }
   render() {
-    let ShowComponent = ["toggler"];
+    let ShowComponent = ["modal", "toggler"];
     if (this.props.show === true) {
       ShowComponent.push("active");
     }
 
     return (
-      <div className={ShowComponent.join(" ")}>
+      <div className={ShowComponent.join(" ")} id="myModal">
+        <div class="modal-content">
+          <div>
+            <span onClick={this.props.close} class="close">
+              &times;
+            </span>
+            <img
+              src={this.getPost(this.props.targetPost).imageUrl}
+              alt="imagefailedtoload"
+              className="VisualizerPicModal"
+            />
+            <h1 className="VisualizerTitleModal">
+              {this.getPost(this.props.targetPost).title}
+            </h1>
+            <p>Written by: Wikipedia</p>
+            <div
+              className="mainViewerP"
+              dangerouslySetInnerHTML={{
+                __html: this.getPost(this.props.targetPost).body,
+              }}
+            ></div>
+            {/* <button onClick={this.props.close}>close</button> */}
+          </div>
+        </div>
         {/* <div>{this.getPost(4).toString()}</div> */}
-        <img
-          src={this.getPost(this.props.targetPost).imageUrl}
-          alt="imagefailedtoload"
-          className="VisualizerPic"
-        />
-        <h1>{this.props.targetPost}</h1>
-        <h6 className="VisualizerTitle">
-          {this.getPost(this.props.targetPost).title}
-        </h6>
-        <div
-          className="mainViewerP"
-          dangerouslySetInnerHTML={{
-            __html: this.getPost(this.props.targetPost).body,
-          }}
-        ></div>
-        <button onClick={this.props.close}>close</button>
       </div>
     );
   }
