a9d04bfd41ed75d585ba0f8a5188a6bd667f3f6d
grambo <gramham@live.com>
2020-05-27 20:16:44 -0600

    quick fix

--- client/src/components/PostContainer/PostContainer.js
+++ client/src/components/PostContainer/PostContainer.js
@@ -3,17 +3,11 @@ import "./PostContainer.css";
 import PostCard from "../PostCard/PostCard";
 import PostViewer from "../PostViewer/PostViewer";
 import { CardDeck, Col, Row } from "react-bootstrap";
-<<<<<<< HEAD
-=======
-import DetailsPage from "../../pages/DetailsPage";
-import _ from 'lodash';
 
->>>>>>> a04f13eeb721364b100ef301edd26c1da27c9b9a
 function PostContainer(props) {
   const Posts = props.posts;
   // let Posts2 = props.posts;
 
-<<<<<<< HEAD
   const [TargetPost, setTargetPost] = useState([]);
   const [showMainViewer, setShowMainViewer] = useState([false]);
 
@@ -37,40 +31,10 @@ function PostContainer(props) {
             //we can fiddle with sizes here :)
             <Col xs="12" sm="6" md="4" lg="2">
               <PostCard indexValue={index} post={post} getid={getid} />
-=======
-  const [selectedPost, setSelectedPost] = useState({});
-
-  const postSelectedHandler = (post, index) => {
-    // alert(`post with the id of ${post._id} has been clicked!`);
-    console.log(index);
-    console.log(selectedPost);
-    // console.log(post);
-    setSelectedPost(post);
-  }
-
-  const loadComponent = () => {
-    if(!_.isEmpty(selectedPost)) {
-      return <DetailsPage post={selectedPost} />
-    }
-
-    return (
-      <div>
-      <CardDeck className="m-1">
-        <Row>
-          {Posts.map((post, index) => (
-            <Col xs="12" sm="6" md="4" lg="2" key={index}>
-              <PostCard 
-                key={index} 
-                indexId={index} 
-                post={post} 
-                postSelectedHandler={postSelectedHandler} 
-              />
->>>>>>> a04f13eeb721364b100ef301edd26c1da27c9b9a
             </Col>
           ))}
         </Row>
       </CardDeck>
-<<<<<<< HEAD
       <PostViewer
         show={showMainViewer}
         posts={Posts}
@@ -79,40 +43,6 @@ function PostContainer(props) {
       />
     </div>
   );
-=======
-      <PostViewer show={false} posts={Posts} />
-      </div>
-    );
-  }
-  
-  return loadComponent();
-  // if(selectedPost.length <= 0) {
-  //   return(
-  //     <DetailsPage post={selectedPost} />
-  //   );
-  // } else {
-  //   return (
-  //     <div>
-  //       <CardDeck className="m-1">
-  //         <Row>
-  //           {Posts.map((post, index) => (
-  //             <Col xs="12" sm="6" md="4" lg="2" key={index}>
-  //               <PostCard 
-  //                 key={index} 
-  //                 indexId={index} 
-  //                 post={post} 
-  //                 postSelectedHandler={postSelectedHandler} 
-  //               />
-  //             </Col>
-  //           ))}
-  //         </Row>
-  //       </CardDeck>
-  //       <PostViewer show={false} posts={Posts} />
-  //     </div>
-  //   );
-  // }
-
->>>>>>> a04f13eeb721364b100ef301edd26c1da27c9b9a
 }
 
 export default PostContainer;
