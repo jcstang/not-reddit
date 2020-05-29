8f685f6fdbd7f5a03d491d9f6a2d6dff452d4471
grambo <gramham@live.com>
2020-05-28 19:38:51 -0600

    added form

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 
-export default class Signup extends Component {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-  render() {
-
-    return(
-      <div>
-        <h1> SignUp Component</h1>
-      </div>
-    );
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <label>Email</label>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <label>Password</label>
+          <FormControl
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 }
