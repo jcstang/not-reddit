732c7deb49437e4b3f1a04ea79f225fdba693f05
Jacob Stanger <jstangwork@gmail.com>
2020-05-30 10:16:52 -0600

    footer implement. styles.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./style.css";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Footer() {
   return (
     <footer>
-      <hr />
+      <Jumbotron>
+        <p className="pull-right">
+          <i className="fab fa-github" /> Brought to you by: Jacob Stanger, Brian Allen, Graham Thomas, and Miranda D'Asto using react.
+        </p>
+
+      </Jumbotron>
+      {/* <hr />
       <p className="pull-right">
         <i className="fab fa-github" /> Brought to you by: Jacob Stanger, Brian Allen, Graham Thomas, and Miranda D'Asto using react.
-      </p>
+      </p> */}
     </footer>
   );
 }
