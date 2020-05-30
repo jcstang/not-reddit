366f957ecb1e9a374e406cd7a76e28d65cfc04f3
Brian Allen <brianallen2401@gmail.com>
2020-05-30 10:44:11 -0600

    corrected merge conflict

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,65 +1,10 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
-export default function Login(props) {
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-=======
 export default class Login extends Component {
->>>>>>> ab6e243d53e79d3611349607e345a8bde7252f9f
 
 
   render() {
 
-<<<<<<< HEAD
-    let userguy = {
-      username: username,
-      email: email,
-      password: password,
-      joinDate: "2020-05-26T03:23:49.058Z",
-      imageUrl: "https://source.unsplash.com/6anudmpILw4/200x200",
-    };
-    props.dispatch({ type: "changeuserdata", placeHolderUser: userguy });
-  }
-
-  return (
-    <div className="Login container">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="username" bsSize="large">
-          <label>Username</label>
-          <FormControl
-            autoFocus
-            type="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="email" bsSize="large">
-          <label>Email</label>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <label>Password</label>
-          <FormControl
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
-}
-=======
     return(
       <div>
         <h1> Login Component</h1>
@@ -67,4 +12,3 @@ export default class Login extends Component {
     );
   }
 }
->>>>>>> ab6e243d53e79d3611349607e345a8bde7252f9f
