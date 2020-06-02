483e1eaa687c057a39842d50e46cc66372a59715
grambo <gramham@live.com>
2020-06-01 20:26:55 -0600

    login upates state

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -15,6 +15,19 @@ export default class Login extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     const { username, password } = this.state;
+    let userguy = {
+      username: username,
+      displayName: "beep",
+      email: username,
+      password: "",
+      joinDate: "2020-05-26T03:23:49.058Z",
+      imageUrl: "https://source.unsplash.com/6anudmpILw4/200x200",
+    };
+
+    this.props.dispatch({
+      type: "changeuserdata",
+      placeHolderUser: userguy,
+    });
     axios({
       url: "api/log-in",
       method: "POST",
