e032e01cd299ed4e8646aa7155d4c0ce37f26820
Jacob Stanger <jacobcstanger@gmail.com>
2020-08-28 14:55:42 -0600

    removed public facing db password

--- config/db.js
+++ config/db.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+// const db = config.get('mongoURI');
 
 const connectDB = async () => {
+
+  let MONGODB_URI = process.env.NODE_ENV
+    ? process.env.MONGODB_URI
+    : "mongodb://localhost/seenit_db";
+
   try {
-    await mongoose.connect(db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
+    // await mongoose.connect(db, {
+    //   useUnifiedTopology: true,
+    //   useNewUrlParser: true
+    // });
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     console.log('MongoDB Connected...');
   } catch (error) {
     console.error(error.message);
@@ -15,5 +24,16 @@ const connectDB = async () => {
     process.exit(1);
   }
 }
+
+// MONGO setup ===
+// =============================================================
+// let MONGODB_URI = process.env.NODE_ENV
+//   ? process.env.MONGODB_URI
+//   : "mongodb://localhost/seenit_db";
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 module.exports = connectDB;
